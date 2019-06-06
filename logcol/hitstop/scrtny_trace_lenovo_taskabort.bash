b29bdea21d99d7dcaca752cdc442d4a8e304625f
Dashi Cao <dscao999@gmail.com>
2019-06-06 16:22:53 +0800

    Use PATH=./bin, remove ./

--- logcol/hitstop/scrtny_trace_lenovo_taskabort.bash
+++ logcol/hitstop/scrtny_trace_lenovo_taskabort.bash
@@ -3,6 +3,7 @@
 #FWBUFSZ=4095
 #Script will finish log collection when hit below event
 ENDMARK="mpt3sas_cm0: _config_request: timeout"
+[ -n "$1" ] && ENDMARK="$1"
 #
 TARGS=$(getopt -l crash,test -o cx -- "$@")
 [ $? -eq 0 ] || exit 1
@@ -40,23 +41,25 @@ echo 0x03f8 >  ${MPT3_DEBUG}
 #
 echo ${MPT3_DEBUG}=$(printf "%#x\n" $(cat ${MPT3_DEBUG}))
 #
+PATH=./bin
+#
 #collect current drive status by scrtnycli tool before test
-./scrtnycli.x86_64 -i 1 cli pl status > pl_status_pretest.txt
-./scrtnycli.x86_64 -i 1 scan > pre_scan.txt
-./dmesg > dmesg_pretest.txt
+scrtnycli.x86_64 -i 1 cli pl status > pl_status_pretest.txt
+scrtnycli.x86_64 -i 1 scan > pre_scan.txt
+dmesg --nopager -k > dmesg_pretest.txt
 
 # Register the Ring Buffer
-#./scrtnycli.x86_64 -i 1 db -register -trace -size ${FWBUFSZ}
+#scrtnycli.x86_64 -i 1 db -register -trace -size ${FWBUFSZ}
 
 #function restore, restore scsi and mpt3sas debug flags when script is stopped
 function restore()
 {
 #  release and unregister trace
-	#./scrtnycli.x86_64 -i 1 db -release -trace
-	#./scrtnycli.x86_64 -i 1 db -unregister -trace
+	#scrtnycli.x86_64 -i 1 db -release -trace
+	#scrtnycli.x86_64 -i 1 db -unregister -trace
 # restore debugging flags
 	echo 0 >  ${MPT3_DEBUG}
-	./sync -f pl_status_pretest.txt
+	sync -f pl_status_pretest.txt
 	if [ -n "$kdump" ]
 	then
 		echo c > /proc/sysrq-trigger
@@ -65,7 +68,7 @@ function restore()
 
 function scrtny_exit()
 {
-	echo "Killed by operator! at $(./date)"
+	echo "Killed by operator! at $(date)"
 	restore
 	exit 0
 }
@@ -78,25 +81,25 @@ numabort=0
 #
 function fwdump()
 {
-	#./scrtnycli.x86_64 -i 1 db -release -trace
-	#./scrtnycli.x86_64 -i 1 db -read -trace -file rb_logs${numabort}.txt
-	#./scrtnycli.x86_64 -i 1 db -unregister -trace
-	./scrtnycli.x86_64 -i 1 cli iop show diag > scrutiny_abort_diag${numabort}.txt
-	./scrtnycli.x86_64 -i 1 cli pl status > pl_status${numabort}.txt
-	./dmesg > kernel${numabort}.txt
-	./sync -f pl_status_pretest.txt
+	#scrtnycli.x86_64 -i 1 db -release -trace
+	#scrtnycli.x86_64 -i 1 db -read -trace -file rb_logs${numabort}.txt
+	#scrtnycli.x86_64 -i 1 db -unregister -trace
+	scrtnycli.x86_64 -i 1 cli iop show diag > scrutiny_abort_diag${numabort}.txt
+	scrtnycli.x86_64 -i 1 cli pl status > pl_status${numabort}.txt
+	dmesg --nopager -k > kernel${numabort}.txt
+	sync -f pl_status_pretest.txt
 	# reRegister the Ring Buffer for next read
-	#./scrtnycli.x86_64 -i 1 db -register -trace -size ${FWBUFSZ}
+	#scrtnycli.x86_64 -i 1 db -register -trace -size ${FWBUFSZ}
 }
 
 #Look for task abort event, when meet "attempting task abort", begin to capture logs
 #
 stplast=0
 count=0
-while ! ./dmesg | ./grep -F "${ENDMARK}"
+while ! dmesg --nopager -k | grep -F "${ENDMARK}"
 do
-	keyline=$(./dmesg|./grep -F "$MARK"|./sed -e '$!d'| \
-			./sed -e 's/^\[ *//' -e 's/\./ /')
+	keyline=$(dmesg --nopager -k |grep -F "$MARK"|sed -e '$!d'| \
+			sed -e 's/^\[ *//' -e 's/\./ /')
 	if [ -n "$keyline" ]
 	then
 		stpcur=${keyline%% *}
@@ -107,23 +110,20 @@ do
 			numabort=$(((numabort+1)%10))
 		fi
 	fi
-	[ ${count} -eq 0 ] && echo "I'm still alive at $(./date)"
-	./sleep
-	count=$(((count+1)%10))
+	[ ${count} -eq 0 ] && echo "I'm still alive at $(date)"
+	sleep
+	count=$(((count+1)%30))
 done
 #
 # Dump 5 more times after timeout
 #
 count=0
-while [ $count -lt 121 ]
+while [ $count -lt 4 ]
 do
-	if [ $((count%30)) -eq 0 ]
-	then
-		./scrtnycli.x86_64 -i 1 cli iop show diag > scrutiny_abort_diag${numabort}.txt
-		./dmesg > kernel${numabort}.txt
-		numabort=$((numabort+1))
-	fi
-	./sleep
+	scrtnycli.x86_64 -i 1 cli iop show diag > scrutiny_abort_diag${numabort}.txt
+	dmesg --nopager -k > kernel${numabort}.txt
+	numabort=$(((numabort+1)%10))
+	sleep 30
 	count=$((count+1))
 done
 #
